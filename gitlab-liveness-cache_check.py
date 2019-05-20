e5173b7e75472b034a44acc91a6dc038fc9e0df3
Urs Rau <TheBigBear@users.noreply.github.com>
2019-05-20 21:07:40 +0200

    got first gitlab-liveness-cache_check to almost work - lets commit

--- gitlab-liveness-cache_check.py
+++ gitlab-liveness-cache_check.py
@@ -1,6 +1,15 @@
-curl -s https://localhost/-/liveness --insecure | \
-sed -e 's/[{}]/''/g' | \
-awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | \
-sed ':a;N;$!ba;s/\n/\t/g' | \
-sed -e 's/[:]/\t/g' | \
-sed -e 's/["]//g'
+import json
+import sys
+from pprint import pprint
+
+jdata = open(sys.argv[1])
+
+data = json.load(jdata)
+
+if data['cache_check']['status'] == 'OK':
+    print ('0 gitlab-liveness-db_check - liveness-db_check status: ' + data['cache_check']['status'])
+else:
+    print ('2 gitlab-liveness-db_check - liveness-db_check status: ' + data['cache_check']['status'])    
+    
+
+jdata.close()
