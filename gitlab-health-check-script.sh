a775d6f1ec1e90eff701117981d7ca2ae008b590
Urs Rau <TheBigBear@users.noreply.github.com>
2019-05-20 19:48:14 +0200

    renamed all bash *.sh script files to python *.py files

--- gitlab-health-check-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-status=`curl -s https://localhost/-/health --insecure`
-
-if [[ $status == *OK* ]]; then
-	echo "0 Gitlab_Health - $status"  
-	else 
-	echo "2 Gitlab_Health - $status" 
-fi 
