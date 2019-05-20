f432b537c1b83cafe618671ef56e6ded226fbd84
Leon Hedding <leon.hedding@om.org>
2019-05-20 21:32:02 +0000

    aligned health_check output with text of other checks

--- gitlab-health-check.sh
+++ gitlab-health-check.sh
@@ -1,4 +1,10 @@
-#!/bin/bash 
+#!/bin/bash
+ 
+status=`curl -s https://localhost/-/health --insecure`
 
-curl -s https://localhost/-/health --insecure
+if [[ $status == *OK* ]]; then
+	echo "0 gitlab-health_check - $status" 2> /dev/null
+	else 
+	echo "2 gitlab-health_check - $status" 2> /dev/null
+fi 
 
