8807df1751e0f24043f993b207545bbe8378df92
TheBigBear <TheBigBear@users.noreply.github.com>
2019-05-20 18:17:25 +0200

    Update gitlab-health-check-script.sh

--- gitlab-health-check-script.sh
+++ gitlab-health-check-script.sh
@@ -1,14 +1,5 @@
 #!/bin/bash 
 
-#alias echo='{ save_flags="$-"; set +x;} 2> /dev/null; echo_and_restore'
-#echo_and_restore() {
-#        builtin echo "$*"
-#        case "$save_flags" in
-#         (*x*)  set -x
-#        esac
-#}
-# 
-
 status=`curl -s https://localhost/-/health --insecure`
 
 if [[ $status == *OK* ]]; then
