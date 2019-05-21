9054bcf35a1d3329da926efd2f0016d9bb02a946
Urs Rau <TheBigBear@users.noreply.github.com>
2019-05-21 07:53:05 +0200

    fix urlopen-test2.py

--- urlopen-test2.py
+++ urlopen-test2.py
@@ -1,34 +1,33 @@
 #!/bin/python3
 
 import sys
-import urllib
-import urllib2
+import urllib.request
 
-urllib.request.urlopen('https://localhost/-/liveness')
-response = response.read()
+with urllib.request.urlopen('https://localhost/-/liveness') as response:
+    response = response.read()
    
-obj = response
+    obj = response
 
-def dump(obj, nested_level=0, output=sys.stdout):
-    spacing = '   '
-    if isinstance(obj, dict):
-        print >> output, '%s{' % ((nested_level) * spacing)
-        for k, v in obj.items():
-            if hasattr(v, '__iter__'):
-                print >> output, '%s%s:' % ((nested_level + 1) * spacing, k)
-                dump(v, nested_level + 1, output)
-            else:
-                print >> output, '%s%s: %s' % ((nested_level + 1) * spacing, k, v)
-        print >> output, '%s}' % (nested_level * spacing)
-    elif isinstance(obj, list):
-        print >> output, '%s[' % ((nested_level) * spacing)
-        for v in obj:
-            if hasattr(v, '__iter__'):
-                dump(v, nested_level + 1, output)
-            else:
-                print >> output, '%s%s' % ((nested_level + 1) * spacing, v)
-        print >> output, '%s]' % ((nested_level) * spacing)
-    else:
-        print >> output, '%s%s' % (nested_level * spacing, obj)
+    def dump(obj, nested_level=0, output=sys.stdout):
+        spacing = '   '
+        if isinstance(obj, dict):
+            print >> output, '%s{' % ((nested_level) * spacing)
+            for k, v in obj.items():
+                if hasattr(v, '__iter__'):
+                    print >> output, '%s%s:' % ((nested_level + 1) * spacing, k)
+                    dump(v, nested_level + 1, output)
+                else:
+                    print >> output, '%s%s: %s' % ((nested_level + 1) * spacing, k, v)
+            print >> output, '%s}' % (nested_level * spacing)
+        elif isinstance(obj, list):
+            print >> output, '%s[' % ((nested_level) * spacing)
+            for v in obj:
+                if hasattr(v, '__iter__'):
+                    dump(v, nested_level + 1, output)
+                else:
+                    print >> output, '%s%s' % ((nested_level + 1) * spacing, v)
+            print >> output, '%s]' % ((nested_level) * spacing)
+        else:
+            print >> output, '%s%s' % (nested_level * spacing, obj)
 
-dump(obj)   
+    dump(obj)   
