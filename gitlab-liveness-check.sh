1ac5ec89e446e2f75f58fa5c46a6d382b9720d5f
Urs Rau <urs.rau@gmail.com>
2019-05-21 12:47:26 +0000

    removed helper *.sh *.sh files

--- gitlab-liveness-check.sh
+++
@@ -1,6 +0,0 @@
-curl -s https://localhost/-/liveness --insecure | \
-sed -e 's/[{}]/''/g' | \
-awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | \
-sed ':a;N;$!ba;s/\n/\t/g' | \
-sed -e 's/[:]/\t/g' | \
-sed -e 's/["]//g'
